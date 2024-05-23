69de638a79c8392784c27c8d5bae2ce3db2fa7cf
Matheus Farias <matheusfarias009@hotmail.com>
2024-05-23 11:28:12 -0300

    Criação de CRUD e banco de dados em memória

---
+++ server.js
@@ -0,0 +1,52 @@
+import { fastify } from 'fastify'
+import { DatabaseMemory } from './database-memory.js'
+
+const server = fastify()
+
+const db = new DatabaseMemory()
+
+
+server.post('/videos', (request, reply) => {
+    const { title, description, duration } = request.body
+
+    db.create({
+       title,
+       description,
+       duration
+    })
+
+    return reply.status(201).send()
+})
+
+server.get('/videos', () => {
+    const videos = db.list()
+
+    console.log(videos)
+
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+    const { title, description, duration } = request.body
+
+    db.update(videoId, {
+        title,
+        description,
+        duration
+    })
+
+    return reply.status(204).send()
+})
+
+server.delete('/videos/:id', (request, reply) => {
+    const videoId = request.params.id
+
+    db.delete(videoId)
+
+    return reply.status(204).send()
+})
+
+server.listen({
+    port: 3333,
+})
